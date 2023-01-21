899f3886552d92152a5a00a1cf2f67aa5369a8bb
otshinra <104638109+otshinra@users.noreply.github.com>
2023-01-21 15:25:24 +0200

    new

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your favorite number? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
     }
 }
